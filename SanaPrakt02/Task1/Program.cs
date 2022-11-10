3e5898addf85d9a6609b53eddf6028a5988870a0
Muravytskyi Vladyslav <ipz223_mvyu@student.ztu.edu.ua>
2022-11-10 17:27:46 +0200

    Зміна readme

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -2,5 +2,5 @@
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Я люблю Житомирську політехніку!");
+Console.WriteLine("Моє місто Житомир!");
